58cf2279e641660e40a81ff1892184670082827d
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 12:19:19 +0200

    Rename + rm comments

---
+++ plugins/elk/src/modules/rdmFsName.ts
@@ -0,0 +1,24 @@
+import { findByProps } from "@vendetta/metro";
+import { before } from "@vendetta/patcher";
+import { storage } from "@vendetta/plugin";
+import randomString from "./import/rdmStr";
+const fileUploadModule = findByProps("uploadLocalFiles");
+
+storage.nameLength ??= 8;
+
+export const onUnload = before("uploadLocalFiles", fileUploadModule, (files) => { 
+    const { fileItems } = files[0];
+    if (!fileItems) return;
+
+    const parsedNameLength = parseInt(storage.nameLength);
+    const length = isNaN(parsedNameLength) ? 8 : parsedNameLength;
+
+    for (const index of fileItems) {
+        const extensionIndex = index.filename.lastIndexOf(".");
+        const fileExtension = extensionIndex !== -1 ? index.filename.slice(extensionIndex) : "";
+
+        const randomFileName = randomString(length);
+        index.filename = randomFileName + fileExtension;
+        if (index.item) index.item.filename = randomFileName + fileExtension;
+    }
+});
