051b5f399de9ad41b8fc6408733269e32af79d39
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 14:40:48 +0200

    Consize + reintroduce nL check

--- plugins/elk/src/modules/rdmFsName.ts
+++ plugins/elk/src/modules/rdmFsName.ts
@@ -4,18 +4,20 @@ import { storage } from "@vendetta/plugin";
 import randomString from "./import/rdmStr";
 const uploadModule = findByProps("uploadLocalFiles");
 
-export const onUnload = before("uploadLocalFiles", uploadModule, (args) => { 
-    const { items: fileList } = args[0];
-    if (!fileList) return;
+storage.nameLength ??= 8;
 
-    const rawNameLength = parseInt(storage.fileNameLength);
-    const fileNameLength = isNaN(rawNameLength) ? 8 : rawNameLength;
+export const onUnload = before('uploadLocalFiles', uploadModule, files => {
+  if (!files[0]) return;
+  const { items: fileList } = files[0];
 
-    for (const file of fileList) {
-        const fileExtensionIndex = file.filename.lastIndexOf(".");
-        const fileExtension = fileExtensionIndex !== -1 ? file.filename.slice(fileExtensionIndex) : "";
-        const newName = randomString(fileNameLength);
-        file.filename = newName + fileExtension;
-        if (file.item) file.item.filename = newName + fileExtension;
-    }
+  const rawNameLength = parseInt(storage.fileNameLength),
+    fileNameLength = isNaN(rawNameLength) ? 8 : rawNameLength;
+
+  for (const file of fileList) {
+    const fileExtensionIndex = file.filename.lastIndexOf('.'),
+      fileExtension = fileExtensionIndex !== -1 ? file.filename.slice(fileExtensionIndex) : '',
+      newName = randomString(fileNameLength);
+    file.filename = newName + fileExtension;
+    if (file.item) file.item.filename = newName + fileExtension;
+  }
 });
