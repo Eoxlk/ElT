6a31762a74bcf6ef42de241d4f9f8a179f9e65df
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 14:13:19 +0200

    Revert to debug

--- plugins/elk/src/modules/import/rdmStr.ts
+++ plugins/elk/src/modules/import/rdmStr.ts
@@ -1,5 +1,2 @@
-import { storage } from '@vendetta/plugin';
-// storage.nameChars ??= "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-
 const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export default (len: number) => [...crypto.getRandomValues(new Uint8Array(len)).map(n => n % chars.length)].map(n => chars.charAt(n)).join('');
--- plugins/elk/src/modules/rdmFsName.ts
+++ plugins/elk/src/modules/rdmFsName.ts
@@ -8,16 +8,18 @@ storage.nameLength ??= 8;
 
 const uploadModule = findByProps("uploadLocalFiles");
 
-export const onUnload = before('uploadLocalFiles', uploadModule, args => {
-  const { items } = args[0];
-  if (!items) return;
-  const rawLength = parseInt(storage.nameLength);
-  const length = isNaN(rawLength) ? 8 : rawLength;
-  for (const i of items) {
-    const extIdx = i.filename.lastIndexOf('.');
-    const ext = extIdx !== -1 ? i.filename.slice(extIdx) : '';
-    const name = randomString(length);
-    i.filename = name + ext;
-    if (i.item) i.item.filename = name + ext;
-  }
+export const onUnload = before("uploadLocalFiles", uploadModule, (args) => { 
+    const { items } = args[0];
+    if (!items) return;
+
+    const rawLength = parseInt(storage.nameLength);
+    const length = isNaN(rawLength) ? 8 : rawLength;
+
+    for (const i of items) {
+        const extIdx = i.filename.lastIndexOf(".");
+        const ext = extIdx !== -1 ? i.filename.slice(extIdx) : "";
+        const name = randomString(length);
+        i.filename = name + ext;
+        if (i.item) i.item.filename = name + ext;
+    }
 });
