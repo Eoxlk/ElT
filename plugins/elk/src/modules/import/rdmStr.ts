982fe5aa7baffaf514d6b769807e3d92bd7919b1
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 14:02:30 +0200

    Update rdmStr.ts

--- plugins/elk/src/modules/import/rdmStr.ts
+++ plugins/elk/src/modules/import/rdmStr.ts
@@ -1,4 +1,4 @@
 import { storage } from '@vendetta/plugin';
-storage.nameChars ??= "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+// storage.nameChars ??= "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
-export default (lenght: number, characters = storage.nameChars) => [...crypto.getRandomValues(new Uint8Array(lenght)).map(n => n % characters.length)].map(n => characters.charAt(n)).join('');
+export default (lenght: number, characters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz") => [...crypto.getRandomValues(new Uint8Array(lenght)).map(n => n % characters.length)].map(n => characters.charAt(n)).join('');
