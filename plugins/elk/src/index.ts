377b287600e485c763d8d3999cfcd0947f75543b
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 14:19:25 +0200

    Attempt to declare nL and nC in index.ts
    Attempt to declare nL and nC in index.ts to prevent "Cannot read property 'toString' of undefined"

--- plugins/elk/src/index.ts
+++ plugins/elk/src/index.ts
@@ -1,5 +1,9 @@
 import Settings from './Settings'
+import { storage } from "@vendetta/plugin";
 import './modules/rdmFsName';
+
+storage.nameLength ??= 8;
+storage.nameChars ??= "abcdefghijklmnopqrstuvwxyz"
 
 export default {
     settings: Settings
