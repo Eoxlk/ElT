4ca49f97e10189a3c8d4ddef66bbc1a37a0b9b8e
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 13:03:13 +0200

    Revert changes

--- plugins/elk/src/modules/import/rdmStr.ts
+++ plugins/elk/src/modules/import/rdmStr.ts
@@ -1,6 +1,2 @@
-const defaultChars = "abcdefghijklmnopqrstuvwxyz";
-
-export default (len: number, chars: string) => {
-  chars = chars || defaultChars;
-  return [...crypto.getRandomValues(new Uint8Array(len)).map(n => n % chars.length)].map(n => chars.charAt(n)).join('');
-};
+const chars = "abcdefghijklmnopqrstuvwxyz";
+export default (len: number) => [...crypto.getRandomValues(new Uint8Array(len)).map(n => n % chars.length)].map(n => chars.charAt(n)).join('');
