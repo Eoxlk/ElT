f4a2e9013f45b6d476010468fc08cce0c149cb95
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 13:37:03 +0200

    remove useless constant in rdmStr.ts

--- plugins/elk/src/modules/import/rdmStr.ts
+++ plugins/elk/src/modules/import/rdmStr.ts
@@ -1,6 +1,3 @@
-const defaultChars = "abcdefghijklmnopqrstuvwxyz";
-
 export default (len: number, chars: string) => {
-  chars = chars || defaultChars;
   return [...crypto.getRandomValues(new Uint8Array(len)).map(n => n % chars.length)].map(n => chars.charAt(n)).join('');
 };
