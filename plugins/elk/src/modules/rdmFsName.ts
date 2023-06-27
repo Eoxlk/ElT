800b35e292df32cce5cb45ca5a9084262ebf0edc
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 12:31:46 +0200

    Concisify

--- plugins/elk/src/modules/rdmFsName.ts
+++ plugins/elk/src/modules/rdmFsName.ts
@@ -1,24 +1,23 @@
-import { findByProps } from "@vendetta/metro";
-import { before } from "@vendetta/patcher";
-import { storage } from "@vendetta/plugin";
-import randomString from "./import/rdmStr";
-const fileUploadModule = findByProps("uploadLocalFiles");
+import { findByProps } from '@vendetta/metro';
+import { before } from '@vendetta/patcher';
+import { storage } from '@vendetta/plugin';
+import randomString from './import/rdmStr';
+let fileUploadModule = findByProps('uploadLocalFiles');
 
 storage.nameLength ??= 8;
 
-export const onUnload = before("uploadLocalFiles", fileUploadModule, (files) => { 
-    const { fileItems } = files[0];
-    if (!fileItems) return;
+export const onUnload = before('uploadLocalFiles', fileUploadModule, files => {
+  if (!files[0]) return;
 
-    const parsedNameLength = parseInt(storage.nameLength);
-    const length = isNaN(parsedNameLength) ? 8 : parsedNameLength;
+  let { fileItems } = files[0],
+    parsedNameLength = parseInt(storage.nameLength),
+    length = isNaN(parsedNameLength) ? 8 : parsedNameLength;
 
-    for (const index of fileItems) {
-        const extensionIndex = index.filename.lastIndexOf(".");
-        const fileExtension = extensionIndex !== -1 ? index.filename.slice(extensionIndex) : "";
-
-        const randomFileName = randomString(length);
-        index.filename = randomFileName + fileExtension;
-        if (index.item) index.item.filename = randomFileName + fileExtension;
-    }
+  for (const index of fileItems) {
+    let extensionIndex = index.filename.lastIndexOf('.'),
+      fileExtension = extensionIndex !== -1 ? index.filename.slice(extensionIndex) : '',
+      randomFileName = randomString(length);
+    index.filename = randomFileName + fileExtension;
+    if (index.item) index.item.filename = randomFileName + fileExtension;
+  }
 });
