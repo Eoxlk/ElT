d47ff9d4798d1b0c929530771b462e492e732471
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 11:29:51 +0200

    index.ts > Add log

--- plugins/elk/src/index.ts
+++ plugins/elk/src/index.ts
@@ -4,6 +4,7 @@ import Settings from "./Settings";
 export default {
     onLoad: () => {
         logger.log("Hello world!");
+        logger.log("Works!")
     },
     onUnload: () => {
         logger.log("Goodbye, world.");
