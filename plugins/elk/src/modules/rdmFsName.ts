868f1476c468755ad961057f60bcefe12f3f92f4
Frops <118558152+Eoxlk@users.noreply.github.com>
2023-06-27 14:28:07 +0200

    Rename variables

--- plugins/elk/src/modules/rdmFsName.ts
+++ plugins/elk/src/modules/rdmFsName.ts
@@ -8,17 +8,17 @@ storage.nameLength ??= 8;
 const uploadModule = findByProps("uploadLocalFiles");
 
 export const onUnload = before("uploadLocalFiles", uploadModule, (args) => { 
-    const { items } = args[0];
-    if (!items) return;
+    const { items: fileList } = args[0];
+    if (!fileList) return;
 
-    const rawLength = parseInt(storage.nameLength);
-    const length = isNaN(rawLength) ? 8 : rawLength;
+    const rawNameLength = parseInt(storage.fileNameLength);
+    const fileNameLength = isNaN(rawNameLength) ? 8 : rawNameLength;
 
-    for (const i of items) {
-        const extIdx = i.filename.lastIndexOf(".");
-        const ext = extIdx !== -1 ? i.filename.slice(extIdx) : "";
-        const name = randomString(length);
-        i.filename = name + ext;
-        if (i.item) i.item.filename = name + ext;
+    for (const file of fileList) {
+        const fileExtensionIndex = file.filename.lastIndexOf(".");
+        const fileExtension = fileExtensionIndex !== -1 ? file.filename.slice(fileExtensionIndex) : "";
+        const newName = randomString(fileNameLength);
+        file.filename = newName + fileExtension;
+        if (file.item) file.item.filename = newName + fileExtension;
     }
 });
