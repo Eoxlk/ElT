cabc86d4a21b686c95ad7881508bb1d130c0cd96
Elk <118558152+Eoxlk@users.noreply.github.com>
2023-10-23 20:50:48 +0200

    thing: maybe now

--- plugins/elk/src/modules/hideGiftButton.ts
+++ plugins/elk/src/modules/hideGiftButton.ts
@@ -1,6 +1,5 @@
 import { findByName, findByProps } from "@vendetta/metro";
 import { after } from "@vendetta/patcher";
-import { getAssetIDByName } from "@vendetta/ui/assets";
 import { findInReactTree } from "@vendetta/utils";
 
 const ChatInput = findByName("ChatInput");
@@ -9,12 +8,10 @@ let unpatch: () => boolean;
 
 export default {
     onLoad() {
-        const blockList = ["ic_thread_normal_24px", "ic_gift"].map(n => getAssetIDByName(n));
         unpatch = after("render", ChatInput.prototype, (_, ret) => {
             const input = findInReactTree(ret, t => "forceAnimateButtons" in t.props && t.props.actions);
-            input.props.CanSendVoiceMessage = false;
-            input.props.hideGiftButton = true;
+            input.props.canSendVoiceMessage = false;
         });
-    },
+    },    
     onUnload: unpatch
 };
