0a4e0d3fd5ade416f4b21cef92c01d1c943f7138
Elk <118558152+Eoxlk@users.noreply.github.com>
2023-10-23 20:40:57 +0200

    chore: format

--- plugins/elk/src/Settings.tsx
+++ plugins/elk/src/Settings.tsx
@@ -6,24 +6,24 @@ import { storage } from "@vendetta/plugin";
 const { FormInput } = Forms;
 
 export default () => {
-    useProxy(storage);
+  useProxy(storage);
 
-    return (
-        <RN.ScrollView>
-            <FormInput
-                title="Length for filenames"
-                keyboardType="numeric"
-                placeholder="0-9"
-                value={storage.nameLength.toString()}
-                onChange={(v: string) => storage.nameLength = v.replace(/[^0-9]/g, "")}
-            />
-            <FormInput
-                title="Characters to use for filenames"
-                keyboardType="default"
-                placeholder="a-z A-z 0-9"
-                value={storage.nameChars.toString()}
-                onChange={(v: string) => storage.nameChars = v.replace(/[^a-zA-Z0-9]+$/, "")}
-            />
-        </RN.ScrollView>
-    )
+  return (
+    <RN.ScrollView>
+      <FormInput
+        title="Length for filenames"
+        keyboardType="numeric"
+        placeholder="0-9"
+        value={storage.nameLength.toString()}
+        onChange={(v: string) => storage.nameLength = v.replace(/[^0-9]/g, "")}
+      />
+      <FormInput
+        title="Characters to use for filenames"
+        keyboardType="default"
+        placeholder="a-z A-z 0-9"
+        value={storage.nameChars.toString()}
+        onChange={(v: string) => storage.nameChars = v.replace(/[^a-zA-Z0-9]+$/, "")}
+      />
+    </RN.ScrollView>
+  )
 }
