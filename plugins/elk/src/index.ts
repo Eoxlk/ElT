baa98a1a813064ad1c6bfb43629ebe8f51cd9fe8
Elk <118558152+Eoxlk@users.noreply.github.com>
2023-10-23 22:04:07 +0200

    je.s..us ..is...nt r.e...al....

--- plugins/elk/src/index.ts
+++ plugins/elk/src/index.ts
@@ -1,28 +1,5 @@
-import Settings from './Settings'
 import { storage } from "@vendetta/plugin";
-import { findByName, findByProps } from '@vendetta/metro';
-import patcher, { after } from '@vendetta/patcher';
-import { getAssetIDByName } from '@vendetta/ui/assets';
-import { findInReactTree } from '@vendetta/utils';
 import './modules/rdmFsName';
 
 storage.nameLength ??= 8;
 storage.nameChars ??= "abcdefghijklmnopqrstuvwxyz"
-
-const ChatInput = findByName("ChatInput");
-const ChatInputWrapper = findByProps("ChatInput");
-
-let unpatch: () => boolean;
-
-export default {
-    settings: Settings,
-    onLoad() {
-        unpatch = patcher.after(ChatInputWrapper.ChatInput, 'render', (_, ret) => {
-            const endpoint: any = findInReactTree(ret, r => typeof r.props?.hideGiftButton === 'boolean');
-            if (!endpoint) return;
-    
-            endpoint.props.hideGiftButton = true;
-        });
-    },
-    onUnload: unpatch
-}
