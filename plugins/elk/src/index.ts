62fec086f5aa224d432da2645c06564ae45d34e5
Elk <118558152+Eoxlk@users.noreply.github.com>
2023-10-23 20:57:46 +0200

    idfk anymore

--- plugins/elk/src/index.ts
+++ plugins/elk/src/index.ts
@@ -1,11 +1,27 @@
 import Settings from './Settings'
 import { storage } from "@vendetta/plugin";
+import { findByName } from '@vendetta/metro';
+import { after } from '@vendetta/patcher';
+import { getAssetIDByName } from '@vendetta/ui/assets';
+import { findInReactTree } from '@vendetta/utils';
 import './modules/rdmFsName';
-import './modules/hideGiftButton';
 
 storage.nameLength ??= 8;
 storage.nameChars ??= "abcdefghijklmnopqrstuvwxyz"
 
+const ChatInput = findByName("ChatInput");
+
+let unpatch: () => boolean;
+
 export default {
-    settings: Settings
+    settings: Settings,
+    onLoad() {
+        const blockList = ["ic_thread_normal_24px", "ic_gift"].map(n => getAssetIDByName(n));
+        unpatch = after("render", ChatInput.prototype, (_, ret) => {
+            const input = findInReactTree(ret, t => "forceAnimateButtons" in t.props && t.props.actions);
+            input.props.CanSendVoiceMessage = false;
+            input.props.hideGiftButton = true;
+        });
+    },
+    onUnload: unpatch
 }
