4ca623b35a6fd8ceb9a6dc242157b2b49a2bf18b
Elk <118558152+Eoxlk@users.noreply.github.com>
2023-10-24 13:35:12 +0200

    retardism

--- plugins/elk/src/index.ts
+++ plugins/elk/src/index.ts
@@ -1,5 +1,11 @@
 import { storage } from "@vendetta/plugin";
+import Settings from "./Settings";
 import './modules/rdmFsName';
 
 storage.nameLength ??= 8;
 storage.nameChars ??= "abcdefghijklmnopqrstuvwxyz"
+
+
+export default {
+    settings: Settings
+}
